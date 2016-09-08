2e486a6bdb3558d47bc07e5af3ded27cc828db1e
nesscollege2016 <nesscollege2016@gmail.com>
2016-09-08 09:28:46 +0300

    Added Test Connectivity

--- app/src/main/java/tomerbu/edu/locationdemos/MainActivity.java
+++ app/src/main/java/tomerbu/edu/locationdemos/MainActivity.java
@@ -2,13 +2,28 @@ package tomerbu.edu.locationdemos;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseUser mCurrentUser;
+    private ValueEventListener mUsersLocationListener;
+    private DatabaseReference mUserLocationsRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +32,58 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+
+        mUsersLocationListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+                if (currentUser != null){
+                    mCurrentUser = currentUser;
+                    onStartedAfterLogin();
+                }else {
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+            }
+        };
+    }
+
+    //Now connected to firebase for sure!
+    private void onStartedAfterLogin() {
+        Toast.makeText(MainActivity.this, "Hello, " + mCurrentUser.getEmail()
+                , Toast.LENGTH_SHORT).show();
+
+        mUserLocationsRef =  FirebaseDatabase.getInstance().getReference().
+                child("UserLocations").child(mCurrentUser.getUid());
+
+        mUserLocationsRef.addValueEventListener(mUsersLocationListener);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthStateListener);
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        FirebaseAuth.getInstance().removeAuthStateListener(mAuthStateListener);
+        mUserLocationsRef.removeEventListener(mUsersLocationListener);
     }
 
     @Override
--- app/src/main/java/tomerbu/edu/locationdemos/MapsActivity.java
+++ app/src/main/java/tomerbu/edu/locationdemos/MapsActivity.java
@@ -1,19 +1,31 @@
 package tomerbu.edu.locationdemos;
 
 import android.app.ProgressDialog;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,22 +33,35 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+
+//https://developer.android.com/training/location/change-location-settings.html#get-settings
 public class MapsActivity extends FragmentActivity
         implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
-        LocationListener{
+        LocationListener {
 
     private static final int REQUEST_CODE_LOCATION = 10;
     private ProgressDialog dialog;
     private GoogleMap map;
     private GoogleApiClient mApiClient;
     private LocationRequest mLocationRequest;
+    private EditText etCity;
+    private EditText etCountry;
+    private EditText etStreet;
+    private int REQUEST_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        etCity = (EditText) findViewById(R.id.etCity);
+        etCountry = (EditText) findViewById(R.id.etCountry);
+        etStreet = (EditText) findViewById(R.id.etStreet);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = new SupportMapFragment();
 
@@ -62,6 +87,7 @@ public class MapsActivity extends FragmentActivity
         super.onStop();
         LocationServices.FusedLocationApi.removeLocationUpdates(mApiClient, this);
     }
+
 
     private void initRadioGroup() {
         RadioGroup rgMapType = (RadioGroup) findViewById(R.id.rgMapType);
@@ -117,10 +143,9 @@ public class MapsActivity extends FragmentActivity
     }
 
 
-
     //Init the Google api client, specifying LocationServices as the api
     private void initApiClient() {
-        mApiClient =  new GoogleApiClient.
+        mApiClient = new GoogleApiClient.
                 Builder(this).
                 addConnectionCallbacks(this).
                 addApi(LocationServices.API).build();
@@ -128,6 +153,7 @@ public class MapsActivity extends FragmentActivity
         mApiClient.connect();
         //when connected?: onConnected
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String[] permissions,
@@ -135,7 +161,7 @@ public class MapsActivity extends FragmentActivity
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         //If we got the permission:
         if (grantResults[0] == PackageManager.PERMISSION_GRANTED
-                && requestCode==REQUEST_CODE_LOCATION){
+                && requestCode == REQUEST_CODE_LOCATION) {
             requestLocation();
         }
     }
@@ -160,9 +186,7 @@ public class MapsActivity extends FragmentActivity
         LocationRequest request = LocationRequest.create();
         request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY).setInterval(1000);
 
-        LocationServices.FusedLocationApi.requestLocationUpdates(mApiClient,
-                request,
-                this);
+        testLocation(request);
 
     }
 
@@ -184,5 +208,90 @@ public class MapsActivity extends FragmentActivity
         LatLng loc = new LatLng(lat, longitude);
 
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 17));
+
+        getAddress(location);
+    }
+
+    //Reverse GeoCoding
+    private void getAddress(Location location) {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            Address address = addresses.get(0);
+
+            StringBuilder builder = new StringBuilder();
+
+            int max = address.getMaxAddressLineIndex();
+            for (int i = 0; i <= max; i++) {
+                builder.append(address.getAddressLine(i)).append("\n");
+            }
+
+            Toast.makeText(MapsActivity.this, builder.toString(), Toast.LENGTH_SHORT).show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void testLocation(final LocationRequest locationRequest) {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(locationRequest);
+        LocationSettingsRequest settingsRequest = builder.build();
+
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(mApiClient, settingsRequest);
+
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(@NonNull LocationSettingsResult res) {
+                Status status = res.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            status.startResolutionForResult(MapsActivity.this, REQUEST_LOCATION);
+                        } catch (IntentSender.SendIntentException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        Toast.makeText(MapsActivity.this, "Location Available", Toast.LENGTH_SHORT).show();
+
+                        startLocationUpdates(locationRequest);
+                        break;
+                    default:
+                        Toast.makeText(MapsActivity.this, "No Location In Device", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+    private void startLocationUpdates(LocationRequest request) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mApiClient,
+                request,
+                this);
+    }
+
+    public void searchAddress(View view) {
+        String city = etCity.getText().toString();
+        String country = etCountry.getText().toString();
+        String address =etStreet.getText().toString();
+
+        Geocoder geocoder = new Geocoder(this);
+        try {
+            List<Address> addressList =
+                    geocoder.getFromLocationName(address + ", " + city + ", " + country, 1);
+
+            Address a = addressList.get(0);
+            LatLng location = new LatLng(a.getLatitude(), a.getLongitude());
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 17));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
